410d384d9cac90b22b933d9c56f584ff81b8574b
vukienHCL <126648920+vukienHCL@users.noreply.github.com>
2023-03-02 13:48:09 +0700

    Add files via upload

--- ESP32_MAX30102.ino
+++ ESP32_MAX30102.ino
@@ -2,12 +2,15 @@
 #include "MAX30105.h" 
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
+#include <string.h>
 
 #define OLED_RESET 0
 Adafruit_SSD1306 display(OLED_RESET);
 MAX30105 particleSensor;
 #define fSpO2 0.7 //filter factor for estimated SpO2
 #define fRate 0.95 //low pass filter for IR/red LED value to eliminate AC component
+#define fbpmrate 0.95 // low pass filter coefficient for HRM in bpm
+#define firrate 0.85 //IR filter coefficient to remove notch , should be smaller than fRate
 float aveRed = 0;//DC component of RED signal
 float aveIr = 0;//DC component of IR signal
 float sumIrRMS = 0; //sum of IR square
@@ -15,7 +18,9 @@ float sumRedRMS = 0; // sum of RED square
 unsigned int counter = 0; //loop counter
 float eSpO2 = 95.0;//initial value of estimated SpO2
 float Ebpm;//estimated Heart Rate (bpm)
-byte validData;      
+byte validData; 
+float Ebpm_Ar[5];
+float eSpO2_Ar[5];
 void initMAX30102() 
 {
   byte  ledBrightness = 0x7F;   //Options: 0=Off to 255=50mA
@@ -40,19 +45,17 @@ void setup()
 }
 float HRM_estimator(float fir ,float aveIr)
 {
-  static float fbpmrate = 0.95; // low pass filter coefficient for HRM in bpm
   static uint32_t crosstime = 0; //falling edge , zero crossing time in msec
   static uint32_t crosstime_prev = 0;//previous falling edge , zero crossing time in msec
   static float bpm = 60.0;
   static float ebpm = 60.0;
   static float eir = 0.0; //estimated lowpass filtered IR signal to find falling edge without notch
-  static float firrate = 0.85; //IR filter coefficient to remove notch , should be smaller than fRate
   static float eir_prev = 0.0;
   // Heart Rate Monitor by finding falling edge
   eir = eir * firrate + fir * (1.0 - firrate); //estimated IR : low pass filtered IR signal
   if ( ((eir - aveIr) * (eir_prev - aveIr) < 0 ) && ((eir - aveIr) < 0.0)) 
-  { //find zero cross at falling edge
-    crosstime = millis();//system time in msec of falling edge
+  { 
+    crosstime = millis();
     if ( ((crosstime - crosstime_prev ) > (60 * 1000 / 180)) && ((crosstime - crosstime_prev ) < (60 * 1000 / 45)) ) 
     {
       bpm = 60.0 * 1000.0 / (float)(crosstime - crosstime_prev) ; //get bpm
@@ -81,9 +84,9 @@ void measure()
     sumRedRMS += (fred - aveRed) * (fred - aveRed); //square sum of alternate component of red level
     sumIrRMS += (fir - aveIr) * (fir - aveIr);//square sum of alternate component of IR level
     Ebpm = HRM_estimator(fir, aveIr); //Ebpm is estimated BPM
-    if (!(counter % 1)) 
+    if (!(counter % 20)) 
     {
-      if ( millis() > 3000) 
+      if ( millis() > 6000) 
       {
         if ( ir < 50000)
         {
@@ -91,11 +94,16 @@ void measure()
         }
         else
         {
-          validData=1;
+          validData=1;    
+          for(int i=0;i<5;i++)
+          {
+            Ebpm_Ar[i]=Ebpm;
+            eSpO2_Ar[i]=eSpO2;
+          } 
         }
       }
     }
-    if ((counter % 100) == 0) 
+    if (!(counter % 100)) 
     {
       float R = (sqrt(sumRedRMS) / aveRed) / (sqrt(sumIrRMS) / aveIr);
       SpO2 = -23.3 * (R - 0.4) + 100 ; 
@@ -119,10 +127,22 @@ void display_human_data()
     display.println("Human data");
     display.setCursor(0,16);
     display.printf("BPM=%0.2f",Ebpm);
-    display.setCursor(24,16);
-    display.print(hr);
     display.setCursor(0,24);
     display.printf("Spo2=%0.2f",eSpO2);
+    char humidity[72]={'\0'};
+    char h1[35];
+    char h2[35];
+    char s[2]={0x5f, 0x5f};
+    char *loc1 = h1;
+    char *loc2 = h2;
+    floatToString(Ebpm_Ar,loc1);
+    floatToString(eSpO2_Ar,loc2);
+    strncat(humidity,h1,35);
+    strncat(humidity,s,2);
+    strncat(humidity,h2,35);
+//    Serial.printf("%s\r\n",h1);
+//    Serial.printf("%s\r\n",h2);
+    Serial.printf("%s\r\n",humidity);
   }
   else
   {
@@ -132,6 +152,18 @@ void display_human_data()
   }
   display.display();
   display.clearDisplay();   
+}
+void floatToString(float testFloats[5], char *loc)
+{
+    char testBuffer[255] = {'\0'};
+    size_t testBufferSpace = 255;
+    size_t tempLen;
+    for(int i = 0; i < 5; ++i)
+    {
+        snprintf(loc, testBufferSpace, "%0.1f ", testFloats[i]);
+        tempLen = strlen(loc);
+        loc += tempLen;
+    }
 }
 void loop()
 {
